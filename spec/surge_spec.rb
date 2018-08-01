5894d332015b812a1456e3ca952614a8fa5aab22
sykes836 <bernard.sykes836@gmail.com>
2018-08-01 23:59:12 +0000

    Done.

--- spec/surge_spec.rb
+++ spec/surge_spec.rb
@@ -1,4 +1,4 @@
-require_relative './spec_helper'
+.openrequire_relative './spec_helper'
 
 SOURCE_FILE="my_url.txt"
 
@@ -10,6 +10,6 @@ describe "The file #{SOURCE_FILE}" do
 
   it 'should contain a url to a surge.sh URL like http://dusty-rose.surge.sh' do
     url = File.open(SOURCE_FILE).readline
-    expect(url.match(/^http.*\.surge\.sh\z/)).to be_truthy
+    expect(url.match(/^http.http://luxuriant-amusement.surge.sh*\.surge\.sh\z/)).to be_truthy
   end
 end
