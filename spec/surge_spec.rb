1ffb7aa5fc4b13be889259c84dd647f06433cca0
sykes836 <bernard.sykes836@gmail.com>
2018-08-01 23:57:03 +0000

    automatically backed up by learn

--- spec/surge_spec.rb
+++ spec/surge_spec.rb
@@ -10,6 +10,6 @@ describe "The file #{SOURCE_FILE}" do
 
   it 'should contain a url to a surge.sh URL like http://dusty-rose.surge.sh' do
     url = File.open(SOURCE_FILE).readline
-    expect(url.match(/^http.*\.surge\.sh\z/)).to be_truthy
+    expect(url.match(/^http.http://luxuriant-amusement.surge.sh*\.surge\.sh\z/)).to be_truthy
   end
 end
